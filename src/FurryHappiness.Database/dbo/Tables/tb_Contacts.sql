b6ea7a49d616643c4374b300837ca66093dd01a7
CI System <gordonbeeming@outlook.com>
2021-09-29 23:20:57 +0000

    added new form

--- src/FurryHappiness.Database/dbo/Tables/tb_Contacts.sql
+++ src/FurryHappiness.Database/dbo/Tables/tb_Contacts.sql
@@ -1,9 +1,10 @@
-CREATE TABLE [dbo].[tb_Contacts] 
+CREATE TABLE [dbo].[tb_Contacts]
 (
   ContactId INT NOT NULL IDENTITY(1,1),
   FirstName NVARCHAR(150) NOT NULL,
-  LastName NVARCHAR(150) NOT NULL,  
+  LastName NVARCHAR(150) NOT NULL,
   EmailAddress NVARCHAR(150) NOT NULL,
+  Age INT NOT NULL,
   CONSTRAINT PK_tb_Contacts_EmailAddress
                PRIMARY KEY CLUSTERED (EmailAddress)
 )
--- src/FurryHappiness/Pages/Index.cshtml.cs
+++ src/FurryHappiness/Pages/Index.cshtml.cs
@@ -5,21 +5,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
 
 namespace FurryHappiness.Pages
 {
   public class IndexModel : PageModel
   {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _config;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public string? FirstName { get; set; }
+    [BindProperty]
+    public string? LastName { get; set; }
+    [BindProperty]
+    public string? EmailAddress { get; set; }
+    [BindProperty]
+    public string? Age { get; set; }
+
+    [ViewData]
+    public string? PageMessage { get; set; }
+    [ViewData]
+    public string? PageError { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
     {
       _logger = logger;
+      _config = config;
     }
 
     public void OnGet()
     {
+    }
 
+    public void OnPost()
+    {
+      try
+      {
+        using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+        {
+          connection.Open();
+          var sql = $@"
+INSERT INTO dbo.[tb_Contacts]
+  (FirstName,LastName,EmailAddress,Age)
+VALUES
+  (@FirstName,@LastName,@EmailAddress,{Age})";
+          using (var cmd = new SqlCommand(sql, connection))
+          {
+            cmd.Parameters.AddWithValue("@FirstName", FirstName);
+            cmd.Parameters.AddWithValue("@LastName", LastName);
+            cmd.Parameters.AddWithValue("@EmailAddress", EmailAddress);
+
+            cmd.ExecuteNonQuery();
+          }
+        }
+        PageMessage = $"Welcome to the club {FirstName}!";
+      }
+      catch (Exception ex)
+      {
+        PageError = ex.Message;
+      }
     }
   }
 }
