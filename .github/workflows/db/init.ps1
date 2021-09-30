5bf022229a1237e7e14e160c07d0b309602aa7b0
CI System <gordonbeeming@outlook.com>
2021-09-30 00:23:42 +0000

    adding PRSonar... and what was meant to be in build

---
+++ .github/workflows/db/init.ps1
@@ -0,0 +1,27 @@
+#requires -PSEdition Core
+
+# Create new database
+# Source for the retry logic:
+# https://stackoverflow.com/a/47712807/411428
+$attempts = 20
+$sleepInSeconds = 3
+Start-Sleep 10 # give it some time before we go in there
+do {
+  try {
+    /opt/mssql-tools/bin/sqlcmd -S ".,1433" -U SA -P $env:SA_PASSWORD -Q "CREATE DATABASE [FurryHappiness]"
+    Write-Host "Database created successfully."
+    break;
+  }
+  catch [Exception] {
+    Write-Host "Retrying... $($_.Exception.Message)"
+  }            
+  $attempts--
+  if ($attempts -gt 0) { Start-Sleep $sleepInSeconds }
+} while ($attempts -gt 0)
+
+# /opt/mssql-tools/bin/sqlcmd -S ".,1433" -U SA -P $env:SA_PASSWORD -Q "RESTORE DATABASE [demo] FROM DISK = '/var/opt/mssql/backup/demo.bak' WITH MOVE 'Demo' TO '/var/opt/mssql/data/demo.mdf', MOVE 'Demo_log' TO '/var/opt/mssql/data/demo.ldf'"
+
+cd /var/opt/mssql/backup
+/var/opt/mssql/sqlpackage/sqlpackage /Action:Publish /SourceFile:FurryHappiness.Database.dacpac /TargetServerName:., 1433 /TargetDatabaseName:FurryHappiness /TargetUser:sa /TargetPassword:$env:SA_PASSWORD /Properties:IncludeCompositeObjects=True
+
+/opt/mssql-tools/bin/sqlcmd -S ".,1433" -U SA -P $env:SA_PASSWORD -d "FurryHappiness" -i "/var/opt/mssql/backup/init.sql"
